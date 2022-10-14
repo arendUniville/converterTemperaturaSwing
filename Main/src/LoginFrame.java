9b63826526a1173bf822759e058f6b3843cba431
Arend <matheus.arend@univille.br>
2022-10-14 20:24:25 -0300

    finished

---
+++ Main/src/LoginFrame.java
@@ -0,0 +1,112 @@
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame {
+
+    private JTextField tempCelsius;
+
+
+    private JTextField senhaField;
+
+
+    private JLabel testeLabel;
+
+    private double celsius = 2.5;
+    private double fahrenheit = 5.0;
+
+    private double resultadoKelvin;
+
+
+
+
+
+    public LoginFrame(){
+
+        setTitle("Converão de temperatura");
+        setSize(300,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        configurarLayout();
+        setVisible(true);
+
+    }
+
+    private void configurarLayout() {
+
+        JPanel painel = new JPanel();
+
+        tempCelsius = new JTextField();
+        tempCelsius.setColumns(15);
+        tempCelsius.setToolTipText("Informe a temperatura");
+
+        senhaField = new JTextField();
+        senhaField.setColumns(15);
+        senhaField.setToolTipText("Informe a senha");
+
+
+
+        JButton botaoKelvin = new JButton("Kelvin");
+        JButton botaoFahrenheit  = new JButton("Fahrenheit");
+
+        // Padrão Observer
+        botaoKelvin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mostrarKelvin();
+            }
+        });
+
+
+        botaoFahrenheit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mostrarFahrenheit();
+            }
+        });
+
+
+
+
+
+        testeLabel = new JLabel();
+
+        painel.add(new JLabel("Indique a temperatura em celsius"));
+        painel.add(tempCelsius);
+        
+        painel.add(botaoKelvin);
+        painel.add(botaoFahrenheit);
+
+        painel.add(testeLabel);
+
+        painel.setBorder(BorderFactory
+                 .createCompoundBorder(BorderFactory.createEmptyBorder(20,20,20,20)
+                 ,BorderFactory.createTitledBorder("Converter Temperatura")));
+
+        //Adicina o painel no JFrame
+        add(painel);
+    }
+
+    private void mostrarKelvin() {
+
+        Integer intCelsius = Integer.parseInt(tempCelsius.getText());  
+
+        double resultadoKelvin = intCelsius + 273.15;
+
+        testeLabel.setText(resultadoKelvin+tempCelsius.getText());
+
+    }
+
+
+    private void mostrarFahrenheit() {
+
+        Integer intCelsius = Integer.parseInt(tempCelsius.getText());  
+
+        double resultadoFahrenheit = (intCelsius * 1.8) + 32;
+
+
+        testeLabel.setText(resultadoFahrenheit+tempCelsius.getText());
+    }
+
+}
